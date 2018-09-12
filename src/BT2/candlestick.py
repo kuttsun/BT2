31130a248964520d305217423ead3583b21651e1
kuttsun <tsukushimail+github@gmail.com>
2018-09-12 13:49:30 +0900

    リファクタリング

--- src/BT2/candlestick.py
+++ src/BT2/candlestick.py
@@ -2,19 +2,27 @@
 # HTTP 通信を行うためのライブラリ
 import requests
 import time
-from datetime import datetime
+from datetime import datetime,timezone
 import datetime as dt
 import pandas as pd
 from matplotlib import pyplot as plt
 import matplotlib.dates as mdates
 from mpl_finance import candlestick_ohlc
+import pytz
 
 # cryptwatch からデータを取得してローソク足を生成
 # https://qiita.com/0x0/items/883fd45c4bd3eb50fcb3
 period = 60
-g_begin = datetime(2018,9,11)
+
+# タイムゾーンを指定しなかった場合、JSTになる
+g_begin = datetime(2018,9,11,0,0,0,0)
+#g_begin = datetime(2018,9,11,0,0,0,0,pytz.timezone('Asia/Tokyo'))
+print(g_begin)
+print(g_begin.timestamp())
 # begin に1日加算
 g_end = g_begin + dt.timedelta(days=1)
+print(g_end)
+print(g_end.timestamp())
 
 # ローソク足を取得する関数
 def get_OHLC(before,after):
@@ -23,21 +31,18 @@ def get_OHLC(before,after):
         # 時間足を秒で指定
         'periods':period,
         # 指定した日時より前のデータを取得
-        'before':before,
+        'before':int(before.timestamp()),
         # 指定した日時より後のデータを取得
-        'after':after,
+        'after':int(after.timestamp()),
     }
     res = requests.get(url,params=query).json()['result'][str(period)]
     return res
-
-# datetime から unixtime へ変換する無名関数
-unixTime = lambda target: int(time.mktime(target.timetuple()))
 
 def download(x):
     begin = g_begin - dt.timedelta(days=x)
     end = g_end - dt.timedelta(days=x)
     # cryptowatch からデータ取得
-    data = get_OHLC(unixTime(end),unixTime(begin))
+    data = get_OHLC(end,begin)
     
     # 実行結果の詳細は以下を参照
     # http://www.hacky.xyz/entry/2018/05/26/205229
