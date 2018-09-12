a795226a1a1f8543709cc99305fbf47d07c31a05
kuttsun <tsukushimail+github@gmail.com>
2018-09-12 09:24:30 +0900

    コメント追加

--- src/BT2/candlestick.py
+++ src/BT2/candlestick.py
@@ -1,4 +1,5 @@
 
+# HTTP 通信を行うためのライブラリ
 import requests
 import time
 from datetime import datetime
@@ -11,16 +12,21 @@ from mpl_finance import candlestick_ohlc
 # cryptwatch からデータを取得してローソク足を生成
 # https://qiita.com/0x0/items/883fd45c4bd3eb50fcb3
 
+# ローソク足を取得する関数
 def get_OHLC(before,after):
     url = 'https://api.cryptowat.ch/markets/bitflyer/btcjpy/ohlc'
     query = {
+        # 時間足を秒で指定
         'periods':60,
+        # 指定した日時より前のデータを取得
         'before':before,
+        # 指定した日時より後のデータを取得
         'after':after,
     }
     res = requests.get(url,params=query).json()['result']['60']
     return res
 
+# 無名関数
 unixTime = lambda y,m,d,h: int(time.mktime(datetime(y,m,d,h).timetuple()))
 
 now = datetime.today()
@@ -29,6 +35,8 @@ text = str(y) + '-' + str(m) + '-' + str(d) + ' ' + str(h) + ':00'
 
 data = get_OHLC(unixTime(y,m,d,h),unixTime(y,m,d,h - 1))
 
+# 実行結果の詳細は以下を参照
+# http://www.hacky.xyz/entry/2018/05/26/205229
 Time,Open,High,Low,Close = [],[],[],[],[]
 for i in data:
     Time.append(i[0])
@@ -37,6 +45,7 @@ for i in data:
     Low.append(i[3])
     Close.append(i[4])
 
+# CSV で出力
 pd.DataFrame({'time':Time, 'open':Open, 'high':High, 'low':Low, 'close':Close}).to_csv('price.csv')
 
 Date = [datetime(y,m,d,h - 1) + dt.timedelta(minutes=mi) for mi in range(60)]
